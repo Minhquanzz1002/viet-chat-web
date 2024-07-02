bec2a453f0dabd2b1c7409a0aa852702345982d7
Nguyễn Minh Quân <quannguyenminh1001@gmail.com>
2024-07-02 17:41:48 +0700

    feat: modal group members (#22)

--- src/api/groupApi.ts
+++ src/api/groupApi.ts
@@ -1,10 +1,18 @@
 import axiosClient from "./axiosClient.ts";
-import {GroupDTO, GroupRequestCreateDTO} from "../models/group.ts";
+import {GroupDTO, GroupMemberDTO, GroupRequestCreateDTO} from "../models/group.ts";
 
 const groupApi = {
     async createGroup(token: string, data: GroupRequestCreateDTO): Promise<GroupDTO> {
         const url: string = '/v1/groups';
         return await axiosClient.post(url, data, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+    },
+    async getAllMembers(token: string, groupId: string): Promise<GroupMemberDTO[]> {
+        const url: string = `/v1/groups/${groupId}/members`;
+        return await axiosClient.get(url, {
             headers: {
                 Authorization: `Bearer ${token}`
             }
